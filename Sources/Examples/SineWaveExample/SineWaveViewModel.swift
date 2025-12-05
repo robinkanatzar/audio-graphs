4d1c6ef6b41ac01ef7b1255b6a0f840711cda671
Naren Krishnaa <narenkrishnaar@gmail.com>
2025-12-06 00:32:17 +0530

    Refactor SineWaveViewModel to initialize properties from AVSineWavePlayer instance

--- Sources/Examples/SineWaveExample/SineWaveViewModel.swift
+++ Sources/Examples/SineWaveExample/SineWaveViewModel.swift
@@ -8,9 +8,25 @@
 import Foundation
 
 final class SineWaveViewModel: ObservableObject {
-  @Published var isPlaying: Bool
-  @Published var frequency: Double
-  @Published var amplitude: Double
+  private var isInitializing = true
+  @Published var isPlaying: Bool {
+    didSet {
+      guard !isInitializing else { return }
+      isPlaying ? player.start() : player.stop()
+    }
+  }
+  @Published var frequency: Double {
+    didSet {
+      guard !isInitializing else { return }
+      player.frequency = frequency
+    }
+  }
+  @Published var amplitude: Double {
+    didSet {
+      guard !isInitializing else { return }
+      player.amplitude = amplitude
+    }
+  }
   private let player: SineWavePlayer
   let frequencyRange: ClosedRange<Double> = 220...880
   let amplitudeRange: ClosedRange<Double> = 0...1
@@ -20,5 +36,6 @@ final class SineWaveViewModel: ObservableObject {
     self.isPlaying = player.isPlaying
     self.frequency = player.frequency
     self.amplitude = player.amplitude
+    self.isInitializing = false
   }
 }
