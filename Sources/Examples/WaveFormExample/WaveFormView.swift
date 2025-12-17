c8575dd04ca63117c82e722882e307c6a0b14fa6
Naren Krishnaa <narenkrishnaar@gmail.com>
2025-12-17 22:42:08 +0530

    Add WaveFormView and WaveFormViewModel as initial components for waveform example

---
+++ Sources/Examples/WaveFormExample/WaveFormView.swift
@@ -0,0 +1,18 @@
+//
+//  WaveFormView.swift
+//  AudioGraphs
+//
+//  Created by Naren on 17/12/25.
+//
+
+import SwiftUI
+
+struct WaveFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaveFormView()
+}
---
+++ Sources/Examples/WaveFormExample/WaveFormViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  WaveFormViewModel.swift
+//  AudioGraphs
+//
+//  Created by Naren on 17/12/25.
+//
+
+import SwiftUI
+
+struct WaveFormViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaveFormViewModel()
+}
