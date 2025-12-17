be27532e2db7856aade597cec01c61ddc0a9af7f
Naren Krishnaa <narenkrishnaar@gmail.com>
2025-12-17 22:45:20 +0530

    Refactor WaveFormViewModel to remove SwiftUI View conformance and simplify structure

--- Sources/Examples/WaveFormExample/WaveFormViewModel.swift
+++ Sources/Examples/WaveFormExample/WaveFormViewModel.swift
@@ -5,14 +5,6 @@
 //  Created by Naren on 17/12/25.
 //
 
-import SwiftUI
-
-struct WaveFormViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    WaveFormViewModel()
+class WaveFormViewModel {
+  
 }
