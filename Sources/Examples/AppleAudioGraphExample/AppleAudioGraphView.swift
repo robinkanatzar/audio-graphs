bcae78b511e1c5ff09536b831c93c3fe7be812a9
Naren Krishnaa <narenkrishnaar@gmail.com>
2025-12-28 18:55:22 +0530

    Add AppleAudioGraphView as a new SwiftUI view for audio graph example

---
+++ Sources/Examples/AppleAudioGraphExample/AppleAudioGraphView.swift
@@ -0,0 +1,18 @@
+//
+//  AppleAudioGraphView.swift
+//  AudioGraphs
+//
+//  Created by Naren on 28/12/25.
+//
+
+import SwiftUI
+
+struct AppleAudioGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppleAudioGraphView()
+}
